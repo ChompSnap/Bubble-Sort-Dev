ec9176d24c93ffc3f573bee68ed3af8468db039f
wvillantay <williamvillantay@gmail.com>
2022-05-12 11:35:47 -0700

    NEW CAMERA
    NEW CAMERA SHAKE

--- BBBP3REB Bubble Sorters/Bubble Sort/Assets/Scripts/Warning.cs
+++ BBBP3REB Bubble Sorters/Bubble Sort/Assets/Scripts/Warning.cs
@@ -31,128 +31,161 @@ public class Warning : MonoBehaviour
             click_count += 1;
 
 
-            if (click_count >= 80)
+            if (click_count == 100)
+            {
+                Debug.Log("Warning_Bubble_Down_100");
+                CameraShake.instance.ShakeEffect();
+                LevelManager.instance.AddNewLine();
+            }
+
+            else if (click_count == 96)
+            {
+                Debug.Log("Warning_Bubble_Down_96");
+                CameraShake.instance.ShakeEffect();
+                LevelManager.instance.AddNewLine();
+            }
+            else if (click_count == 92)
+            {
+                Debug.Log("Warning_Bubble_Down_92");
+                CameraShake.instance.ShakeEffect();
+                LevelManager.instance.AddNewLine();
+            }
+            else if (click_count == 88)
+            {
+                Debug.Log("Warning_Bubble_Down_88");
+                CameraShake.instance.ShakeEffect();
+                LevelManager.instance.AddNewLine();
+            }
+            else if (click_count == 84)
+            {
+                Debug.Log("Warning_Bubble_Down_84");
+                CameraShake.instance.ShakeEffect();
+                LevelManager.instance.AddNewLine();
+            }
+            else if (click_count == 80)
             {
                 Debug.Log("Warning_Bubble_Down_80");
-                bubble_warning.position = new Vector2(transform.position.x, -10f);
+                CameraShake.instance.ShakeEffect();
+                LevelManager.instance.AddNewLine();
             }
-            else if (click_count >= 76)
+            else if (click_count == 76)
             {
                 Debug.Log("Warning_Bubble_Down_76");
-                bubble_warning.position = new Vector2(transform.position.x, -9.5f);
+                CameraShake.instance.ShakeEffect();
+                LevelManager.instance.AddNewLine();
             }
-            else if (click_count >= 72)
+
+            else if (click_count == 72)
             {
                 Debug.Log("Warning_Bubble_Down_72");
-                bubble_warning.position = new Vector2(transform.position.x, -9f);
+                CameraShake.instance.ShakeEffect();
+                LevelManager.instance.AddNewLine();
             }
-
-            else if (click_count >= 68)
+            else if (click_count == 68)
             {
                 Debug.Log("Warning_Bubble_Down_68");
-                bubble_warning.position = new Vector2(transform.position.x, -8.5f);
+                CameraShake.instance.ShakeEffect();
+                LevelManager.instance.AddNewLine();
             }
-
-            else if (click_count >= 64)
+            else if (click_count == 64)
             {
                 Debug.Log("Warning_Bubble_Down_64");
-                bubble_warning.position = new Vector2(transform.position.x, -8f);
+                CameraShake.instance.ShakeEffect();
+                LevelManager.instance.AddNewLine();
             }
-
-            else if (click_count >= 60)
+            else if (click_count == 60)
             {
                 Debug.Log("Warning_Bubble_Down_60");
-                bubble_warning.position = new Vector2(transform.position.x, -7.5f);
+                CameraShake.instance.ShakeEffect();
+                LevelManager.instance.AddNewLine();
             }
-
-
-            else if (click_count >= 56)
+            else if (click_count == 56)
             {
                 Debug.Log("Warning_Bubble_Down_56");
-                bubble_warning.position = new Vector2(transform.position.x, -7f);
+                CameraShake.instance.ShakeEffect();
+                LevelManager.instance.AddNewLine();
             }
-
-
-            else if (click_count >= 52)
+            else if (click_count == 52)
             {
                 Debug.Log("Warning_Bubble_Down_52");
-                bubble_warning.position = new Vector2(transform.position.x, -6.5f);
+                CameraShake.instance.ShakeEffect();
+                LevelManager.instance.AddNewLine();
             }
 
-
-            else if (click_count >= 48)
+            else if (click_count == 48)
             {
                 Debug.Log("Warning_Bubble_Down_48");
-                bubble_warning.position = new Vector2(transform.position.x, -6f);
+                CameraShake.instance.ShakeEffect();
+                LevelManager.instance.AddNewLine();
             }
-
-
-            else if (click_count >= 44)
+            else if (click_count == 44)
             {
                 Debug.Log("Warning_Bubble_Down_44");
-                bubble_warning.position = new Vector2(transform.position.x, -5.5f);
+                CameraShake.instance.ShakeEffect();
+                LevelManager.instance.AddNewLine();
             }
-
-
-            else if (click_count >= 40)
+            else if (click_count == 40)
             {
                 Debug.Log("Warning_Bubble_Down_40");
-                bubble_warning.position = new Vector2(transform.position.x, -5f);
+                CameraShake.instance.ShakeEffect();
+                LevelManager.instance.AddNewLine();
             }
-
-
-            else if (click_count >= 36)
+            else if (click_count == 36)
             {
                 Debug.Log("Warning_Bubble_Down_36");
-                bubble_warning.position = new Vector2(transform.position.x, -4.5f);
+                CameraShake.instance.ShakeEffect();
+                LevelManager.instance.AddNewLine();
             }
-
-
-            else if (click_count >= 32)
+            else if (click_count == 32)
             {
                 Debug.Log("Warning_Bubble_Down_32");
-                bubble_warning.position = new Vector2(transform.position.x, -4f);
+                CameraShake.instance.ShakeEffect();
+                LevelManager.instance.AddNewLine();
             }
-
-
-            else if (click_count >= 28)
+            else if (click_count == 28)
             {
                 Debug.Log("Warning_Bubble_Down_28");
-                bubble_warning.position = new Vector2(transform.position.x, -3.5f);
+                CameraShake.instance.ShakeEffect();
+                LevelManager.instance.AddNewLine();
             }
 
-
-            else if (click_count >= 24)
+            else if (click_count == 24)
             {
                 Debug.Log("Warning_Bubble_Down_24");
-                bubble_warning.position = new Vector2(transform.position.x, -3f);
+                CameraShake.instance.ShakeEffect();
+                LevelManager.instance.AddNewLine();
             }
-            else if (click_count >= 20)
+            else if (click_count == 20)
             {
                 Debug.Log("Warning_Bubble_Down_20");
-                bubble_warning.position = new Vector2(transform.position.x, -2.5f);
+                CameraShake.instance.ShakeEffect();
+                LevelManager.instance.AddNewLine();
             }
-            else if (click_count >= 16)
+            else if (click_count == 16)
             {
                 Debug.Log("Warning_Bubble_Down_16");
-                bubble_warning.position = new Vector2(transform.position.x, -2f);
+                CameraShake.instance.ShakeEffect();
+                LevelManager.instance.AddNewLine();
             }
-            else if (click_count >= 12)
+            else if (click_count == 12)
             {
                 Debug.Log("Warning_Bubble_Down_12");
-                bubble_warning.position = new Vector2(transform.position.x, -1.5f);
+                CameraShake.instance.ShakeEffect();
+                LevelManager.instance.AddNewLine();
             }
-            else if (click_count >= 8)
+            else if (click_count == 8)
             {
                 Debug.Log("Warning_Bubble_Down_8");
-                bubble_warning.position = new Vector2(transform.position.x, -1f);
+                CameraShake.instance.ShakeEffect();
+                LevelManager.instance.AddNewLine();
             }
-            else if (click_count >= 4)
+            else if (click_count == 4)
             {
                 Debug.Log("Warning_Bubble_Down_4");
-                bubble_warning.position = new Vector2(transform.position.x, -0.5f);
+                CameraShake.instance.ShakeEffect();
+                LevelManager.instance.AddNewLine();
             }
 
-        }
+        }//bubble_warning.position = new Vector2(transform.position.x, -0.5f);
     }
 }
